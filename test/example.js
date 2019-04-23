e83da01f3e15bc1b038be0803f25cf29fa6aacef
wangqingzhu <wangqingzhu@lanjingren.com>
2019-04-23 16:36:20 +0800

    add rules await

--- test/example.js
+++ test/example.js
@@ -2,3 +2,28 @@
  * Reset test's example
  * Empty rule's for index.js
  */
+
+async function test() {
+  let a = 1
+  let ab = 10
+  let b = 1
+  var c = 2
+
+  console.log(a, b)
+  if (a === 2) {
+    console.log(a)
+    console.log(c)
+  }
+  return await new Promise()
+}
+test()
+
+class A {
+  constructor() {
+    this.a = 1
+  }
+
+  test() {
+
+  }
+}
